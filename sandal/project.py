d5d6d1f69992bb71b13d0df8f52066ef49859ff9
Michael Ekstrand <md@ekstrandom.net>
2024-05-31 12:24:46 -0400

    reduce external deps

--- sandal/project.py
+++ sandal/project.py
@@ -7,14 +7,30 @@ import os
 import sys
 from pathlib import Path
 
-from pyprojroot import find_root, has_dir, here
+__all__ = ["project_root", "setup_project_dir"]
 
-__all__ = ["here", "setup_project_dir"]
-
+_initialized: bool = False
 _root_dir: Path
 
 
 def setup_project_dir():
-    global _root_dir
-    _root_dir = find_root(has_dir(".git"))
+    global _root_dir, _initialized
+    if _initialized:
+        return
+
+    cur = Path.cwd()
+    while cur.parent and not (cur / ".git").exists():
+        cur = cur.parent
+
+    if (cur / ".git").exists():
+        _root_dir = cur
+    else:
+        raise RuntimeError("cannot find project root directory")
     sys.path.insert(0, os.fspath(_root_dir))
+    _initialized = True
+
+
+def project_root() -> Path:
+    "Get the project root directory."
+    setup_project_dir()
+    return _root_dir
